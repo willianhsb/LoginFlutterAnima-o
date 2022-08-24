81312cef80d4d25c8d99736c783998ce6e7d30bd
Willian <willianhsb@gmail.com>
2022-08-24 19:20:05 -0300

    240822-hom-top

--- lib/screen/home/widgets/stagger_animation.dart
+++ lib/screen/home/widgets/stagger_animation.dart
@@ -1,7 +1,8 @@
-// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors_in_immutables
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors_in_immutables, prefer_const_constructors
 
 import 'package:flutter/material.dart';
 
+import 'animated_list_view.dart';
 import 'home_top.dart';
 
 class StaggerAnimation extends StatelessWidget {
@@ -11,9 +12,24 @@ class StaggerAnimation extends StatelessWidget {
       : containerGrow = CurvedAnimation(
           parent: controller,
           curve: Curves.ease,
+        ),
+        listSlidePosition = EdgeInsetsTween(
+          begin: EdgeInsets.only(bottom: 0),
+          end: EdgeInsets.only(bottom: 80),
+        ).animate(
+          CurvedAnimation(
+            parent: controller,
+            curve: Interval(
+              //porcentagem de tempo das animações
+              0.325, //32% de 2 segundos que é o tempo da animação.
+              0.8, //80% de 2 segundos que é o tempo da animação.
+              curve: Curves.ease,
+            ),
+          ),
         );
 
   final Animation<double> containerGrow;
+  final Animation<EdgeInsets> listSlidePosition;
 
   Widget _buildAnimation(BuildContext context, Widget? child) {
     return ListView(
@@ -21,6 +37,9 @@ class StaggerAnimation extends StatelessWidget {
       children: <Widget>[
         HomeTop(
           containerGrow: containerGrow,
+        ),
+        AnimatedListView(
+          listSlidePosition: listSlidePosition,
         ),
       ],
     );
